8820fe23009757a54b4c14c7862101ca925a8b71
Enderpeanut-SZY <enderpeanut@gmail.com>
2024-12-27 00:41:17 +0800

    Update locate.py

--- locate.py
+++ locate.py
@@ -4,13 +4,15 @@ locations = []
 target_string = input("请输入消息对象名称(备注): ")
 
 with open("input.txt", "r", encoding="utf-8") as file:
+    line_count = 0
     for line_number, line in enumerate(file, start=1):
+        line_count += 1
         if line.startswith("=") and len(line.strip())==64 and set(line.strip())=={"="}:
             line_order.append(line_number)
-for i in range(len(line_order)-2):
-    if line_order[i+1]-line_order[i]==2 and line_order[i+2]-line_order[i+1]==2:
-        locations.append(line_order[i]+3)
-locations.append(4)
+    for i in range(len(line_order)-2):
+        if line_order[i+1]-line_order[i]==2 and line_order[i+2]-line_order[i+1]==2:
+            locations.append(line_order[i]+3)
+    locations.append(line_count+4)
 
 with open("input.txt", "r", encoding="utf-8") as file:
     pos = 0
@@ -21,7 +23,7 @@ with open("input.txt", "r", encoding="utf-8") as file:
             if line.strip() == f"消息对象:{target_string}":
                 print(f"\n消息对象: {target_string} 的消息行数范围为\n{locations[pos-1]+2}\n至\n{locations[pos]-4}")
                 found = True
-if not found: print(f"未找到消息对象: {target_string}")
+    if not found: print(f"未找到消息对象: {target_string}")
 
 
 
